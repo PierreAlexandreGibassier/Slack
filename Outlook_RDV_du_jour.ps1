38d4d4656e3e46385fe4b783c92551ad341448dc
PierreAlexandreGibassier <140069452+PierreAlexandreGibassier@users.noreply.github.com>
2023-07-20 12:58:26 +0200

    Update Outlook_RDV_du_jour.ps1

--- Outlook_RDV_du_jour.ps1
+++ Outlook_RDV_du_jour.ps1
@@ -1,4 +1,34 @@
-﻿. .\FonctionSlack.ps1
+function EnvoyerMessage {
+     param(
+        [string]$cnl,
+        [string]$msg
+    )
+    $slackToken = "xoxb-5431437001842-5572288696579-ZqtDwwgKwW7KW8bRJFjuEkZc"
+    $sortie=""
+
+    $message = @{
+        channel = $cnl
+        text = $msg
+        link_names = $true
+        mrkdwn = $true
+    } | ConvertTo-Json
+
+    $url = "https://slack.com/api/chat.postMessage"
+
+    $headers = @{
+        "Authorization" = "Bearer $slackToken"
+        "Content-Type" = "application/json"
+    }
+
+    $response = Invoke-RestMethod -Uri $url -Headers $headers -Method Post -Body $message
+
+    if ($response.ok) {
+        $sortie = "Message envoyé avec succès à Slack."
+    } else {
+        $sortie = "Erreur lors de l'envoi du message à Slack : $($response.error)"
+    }
+    return $sortie
+}
 
 # Charger le module Outlook
 Add-Type -AssemblyName "Microsoft.Office.Interop.Outlook"
