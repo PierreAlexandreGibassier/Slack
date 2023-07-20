9498f5814681d493705385b33097ee025164e2a9
PierreAlexandreGibassier <140069452+PierreAlexandreGibassier@users.noreply.github.com>
2023-07-20 13:02:24 +0200

    Outlook_RDV_du_jour.ps1

--- Outlook_RDV_du_jour.ps1
+++ Outlook_RDV_du_jour.ps1
@@ -3,7 +3,7 @@ function EnvoyerMessage {
         [string]$cnl,
         [string]$msg
     )
-    $slackToken = "xoxb-5431437001842-5572288696579-qFh6W3S46L1PFyeYVjyrrIOc"
+    $slackToken = "xoxb-5431437001842-5572288696579-ZimVBRzqQCiLoTezLktMXB9Y"
     $sortie=""
 
     $message = @{
